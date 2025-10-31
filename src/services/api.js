f18fcb00259667fec4f288a59b69cab0965db811
mohamedaliarafat <112913473+mohamedaliarafat@users.noreply.github.com>
2025-10-31 23:40:36 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://back-end-albuhairaadmenpanel.onrender.com'; // رابط الـ backend
+const API_URL = 'https://admin-panel-albuhaira-alarabia.onrender.com'; // رابط الـ backend
 
 const api = axios.create({
   baseURL: API_URL,
